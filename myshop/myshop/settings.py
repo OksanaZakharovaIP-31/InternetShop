982ca5c76ba732706f436767ac39b4d764de3c6f
Oksana <oksana2000-02@mail.ru>
2023-10-15 21:18:08 +0300

    add send mail

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -132,4 +132,12 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CART_SESSION_ID = 'card'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+EMAIL_SERVER = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- myshop/orders/tasks.py
+++ myshop/orders/tasks.py
@@ -1,6 +1,8 @@
 from celery import shared_task
 from django.core.mail import send_mail
 from .models import Order
+from django.conf import settings
+
 
 @shared_task
 def order_created(order_id):
@@ -8,12 +10,17 @@ def order_created(order_id):
     Задание по отправке уведомления по электронной почте
     при успешном создании заказа.
     """
+    print('here')
     order = Order.objects.get(id=order_id)
     subject = f'Order nr. {order.id}'
     message = f'Dear {order.first_name},\n\nYou have successfully placed an order. ' \
               f'Your order ID is {order.id}.'
-    mail_sent = send_mail(subject,
-                          message,
-                          'admin@myshop.com',
-                          [order.email])
-    return mail_sent
+    send_mail(subject,
+              message,
+              settings.EMAIL_HOST_USER,
+              [order.email])
+    # mail_sent = send_mail(subject,
+    #                       message,
+    #                       settings.EMAIL_HOST_USER,
+    #                       [order.email])
+    return None # mail_sent
